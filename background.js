f77cd6c433f5ab902aecacb5b02a7e24792d657c
Derek <dk.derek.koh@gmail.com>
2023-07-06 00:44:33 -0500

    got a pop up

---
+++ background.js
@@ -0,0 +1,28 @@
+// entertainment websites:
+const badSites = [
+  'youtube.com',
+  'reddit.com',
+  'facebook.com',
+  'twitter.com',
+  'instagram.com',
+  'tiktok.com',
+  'netflix.com',
+  'crunchyroll.com',
+  '9gag.com',
+  'hulu.com',
+  'primevideo.com',
+];
+
+//Overall Logic
+//Event Listener listening for creation of new tab containing any part of the badSites URL's
+
+// const newPerms = { permissions: badSites };
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  for (const site of badSites) {
+    if (tab.url.includes(site)) {
+      chrome.windows.create({ url: './app/index.html', type: 'popup' });
+      break;
+    }
+  }
+});
---
+++ index.js
@@ -0,0 +1,17 @@
+async function getCatBoy() {
+  const response = await fetch('https://api.catboys.com/baka');
+  const data = await response.json();
+  const baka = data.url;
+  const musicUrl = ['airhorn.mp3'];
+
+  console.log(`HELLO PLS RETURN ${baka}`);
+
+  document.querySelector('.cat').innerHTML = `
+<img id='baka' src="${baka}" alt="baka img">`;
+  document.querySelector(
+    '.audio'
+  ).innerHTML = `<audio id="music" controls autoplay loop>
+<source src="./music/${musicUrl[0]}" />`;
+}
+
+getCatBoy();
