f431f340cc8456f8d1da9c9901b09edc9239a68c
Derek <dk.derek.koh@gmail.com>
2023-07-06 09:50:45 -0500

    added music, timer, and adjusted window pop up amount

--- background.js
+++ background.js
@@ -17,12 +17,26 @@ const badSites = [
 //Event Listener listening for creation of new tab containing any part of the badSites URL's
 
 // const newPerms = { permissions: badSites };
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  for (const site of badSites) {
-    if (tab.url.includes(site)) {
-      chrome.windows.create({ url: './app/index.html', type: 'popup' });
-      break;
+let time = 5;
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    for (const site of badSites) {
+      if (tab.url.includes(site)) {
+        const intervalId = setInterval(() => {
+          time--;
+          if (time === 0) {
+            clearInterval(intervalId);
+            chrome.windows.create({
+              url: './app/index.html',
+              type: 'popup',
+              width: 1750,
+              height: 1200,
+            });
+          }
+        }, 1000);
+        time = 5;
+        break;
+      }
     }
   }
 });
--- index.js
+++ index.js
@@ -2,16 +2,15 @@ async function getCatBoy() {
   const response = await fetch('https://api.catboys.com/baka');
   const data = await response.json();
   const baka = data.url;
-  const musicUrl = ['airhorn.mp3'];
 
-  console.log(`HELLO PLS RETURN ${baka}`);
+  const random = Math.floor(Math.random() * 6) + 1;
 
   document.querySelector('.cat').innerHTML = `
 <img id='baka' src="${baka}" alt="baka img">`;
   document.querySelector(
     '.audio'
   ).innerHTML = `<audio id="music" controls autoplay loop>
-<source src="../music/airhorn/.mp3" />
+<source src="../music/${random}.mp3" />
 </audio>`;
 }
 
